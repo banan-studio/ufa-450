be58ff1239dc284eb06827143e3c84e39edfc67f
Yakin Nikita <yakin95@gmail.com>
2023-09-04 10:44:26 +0500

    feat: nuxt-simple-robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 	experimental: {
 		payloadExtraction: false
 	},
-	modules: ['@pinia/nuxt', '@vueuse/nuxt', 'nuxt-simple-sitemap', '@nuxtjs/google-fonts', '@nuxt/image', 'nuxt-gtag'],
+	modules: ['@pinia/nuxt', '@vueuse/nuxt', 'nuxt-simple-sitemap', '@nuxtjs/google-fonts', '@nuxt/image', 'nuxt-gtag', 'nuxt-simple-robots'],
 	googleFonts: {
 		families: {
 			Inter: [500, 600]
